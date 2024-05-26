b723a29c032924102da2779c3082dfa3d22a52be
kuldeep Raj Gour <kuldeepgour002@gmail.com>
2024-05-26 14:48:10 +0530

    added routes and utils for error

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,7 @@
 const express = require('express');
+const router = express.Router();
 
 const v1ApiRoutes = require('./v1/index');
 
-const router = express.Router();
-
-router.use('/v1',v1ApiRoutes);
-
+router.use('/v1', v1ApiRoutes);
 module.exports = router;
